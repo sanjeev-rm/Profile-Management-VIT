ad08fda8ea276cd29c27cceb0ccec8917c2f2540
sanjeev-rm <sanjeevraghu2050@gmail.com>
2022-10-16 09:56:35 +0530

    Handled exception in the inputDate() method in Date class.

--- Profile Management for VITIANS/src/profile/Date.java
+++ Profile Management for VITIANS/src/profile/Date.java
@@ -1,24 +1,53 @@
 package profile;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.time.DateTimeException;
 
 public class Date
 {
+	
+	public static void main(String[] args)
+	{
+		LocalDate date = Date.inputDate();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-YYYY");
+		System.out.println(date.format(dtf));
+	}
+	
 	public static LocalDate inputDate()
 	{
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.printf("%-30s : ", "YEAR");
-		int year = sc.nextInt();
-		
-		System.out.printf("%-30s : ", "MONTH");
-		int month = sc.nextInt();
-		
-		System.out.printf("%-30s : ", "DAY");
-		int day = sc.nextInt();
-		
-		LocalDate date = LocalDate.of(year, month, day);
-		return date;
+		while(true)
+		{
+			Scanner sc = new Scanner(System.in);
+			
+			try
+			{
+				System.out.printf("%-30s : ", "YEAR");
+				int year = sc.nextInt();
+				
+				System.out.printf("%-30s : ", "MONTH");
+				int month = sc.nextInt();
+				
+				System.out.printf("%-30s : ", "DAY");
+				int day = sc.nextInt();
+				
+				LocalDate date = LocalDate.of(year, month, day);
+				return date;
+			}
+			catch(InputMismatchException ime)
+			{
+				System.out.println("<Please enter only integers for date inputs>");
+			}
+			catch(DateTimeException dte)
+			{
+				System.out.println("<Please enter valid date inputs [month : 1 - 12] [day : 1 - 31]>");
+			}
+			finally
+			{
+				sc.nextLine();
+			}
+		}
 	}
 }
