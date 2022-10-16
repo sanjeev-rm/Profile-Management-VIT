695f36073e7a5ddf18ebd0528fb62c97fdca5dbf
sanjeev-rm <sanjeevraghu2050@gmail.com>
2022-10-16 11:18:54 +0530

    Added / Edited comments.

--- Profile Management for VITIANS/src/exceptions/OptionInvalidException.java
+++ Profile Management for VITIANS/src/exceptions/OptionInvalidException.java
@@ -2,7 +2,7 @@ package exceptions;
 
 /**
  * Exception that is thrown when the option entered is invalid.
- * Eg: This exception is thrown when the options valid are like 1 to 12 but the user eneters -1 or 13 or something like that.
+ * Eg: This exception is thrown when the options valid are like 1 to 12 but the user enters -1 or 13 or something like that.
  * @author sanjeev.rm
  *
  */
