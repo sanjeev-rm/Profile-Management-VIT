f6911a63a3b328680d3064d36ceb60c2ed8c2646
sanjeev-rm <sanjeevraghu2050@gmail.com>
2022-10-16 09:36:09 +0530

    Finished the inputDate() method in Date class.

--- Profile Management for VITIANS/src/profile/Date.java
+++ Profile Management for VITIANS/src/profile/Date.java
@@ -1,5 +1,24 @@
 package profile;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Date
 {
+	public static LocalDate inputDate()
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.printf("%-30s : ", "YEAR");
+		int year = sc.nextInt();
+		
+		System.out.printf("%-30s : ", "MONTH");
+		int month = sc.nextInt();
+		
+		System.out.printf("%-30s : ", "DAY");
+		int day = sc.nextInt();
+		
+		LocalDate date = LocalDate.of(year, month, day);
+		return date;
+	}
 }
